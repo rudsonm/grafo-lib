2047ef3e880bd4d741c4ff6d3086b2932d998860
rudsonm <rudsonm@edu.univali.br>
2017-10-04 15:54:34 -0300

    implementação coloração

--- js/grafo.utils.js
+++ js/grafo.utils.js
@@ -123,3 +123,76 @@ function dijkstra(matriz, atual, destino) {
     } while(existeVerticeAbertoFinito);
     return vertices;
 }
+
+function coloracaoModelBuild(grafo) {
+	var coloracaoModel = [];
+	for(var vertice in grafo.V) {
+		var posicao = grafo.retornarPosicaoVertice(vertice);
+		coloracaoModel.push({
+			posicao: posicao,
+			nome: vertice,
+			grau: grafo.obterVizinhos(posicao).length,
+			saturacao: 0,
+			cor: null			
+		});
+	}
+	coloracaoModel.sort((a, b) => b - a);
+	return coloracaoModel;
+}
+
+function existeVizinhoComMesmaCor(grafo, atual, vertices, cor) {
+	for(var vertice in vertices) {
+		var vizinhos = Boolean(grafo.a[atual.posicao][vertice.posicao]);
+		if(vizinhos && vertice.cor === cor)
+			return true;
+	}
+	return false;
+}
+
+function obterCoresPadrao() {
+	return ['#2ecc71', '#f1c40f', '#2980b9', '#e74c3c', '#2c3e50'];
+}
+
+function welshPowell(grafo, cores) {
+	cores = cores || obterCoresPadrao();
+	var verticesNaoColoridos = coloracaoModelBuild(grafo);
+	var verticesColoridos = [];
+	var coresUtilizadas = 0;
+	do {
+		var corAtual = cores.shift();
+		verticesNaoColoridos.forEach(function(vertice, indice) {
+			if(!existeVizinhoComMesmaCor(grafo, vertice, verticesColoridos, corAtual)) {
+				vertice.cor = corAtual;
+				verticesNaoColoridos.splice(indice, 1);
+				verticesColoridos.push(vertice);
+			}
+		});
+	} while(verticesNaoColoridos.length && cores.length);
+	return verticesColoridos;
+}
+
+function dSatur(grafo, cores) {
+	cores = cores || obterCoresPadrao();
+	var verticesNaoColoridos = coloracaoModelBuild(grafo);
+	var verticesColoridos = [verticesNaoColoridos.shift()];
+	verticesColoridos[0].cor = cores[0];
+	do {
+		var vertice = verticesNaoColoridos[0];
+		var indice = 0;
+		for(let i = 1; i < verticesNaoColoridos; i++) {
+			if(verticesNaoColoridos[i].saturacao > vertice.saturacao || verticesNaoColoridos[i].grau > vertice.grau) {
+				vertice = verticesNaoColoridos[i];
+				indice = i;
+			}
+		}
+		for(let cor in cores) {
+			if(!existeVizinhoComMesmaCor(grafo, vertice, verticesColoridos, cor)) {
+				vertice.cor = cor;
+				verticesNaoColoridos.splice(indice, 1);
+				verticesColoridos.push(vertice);
+				break;
+			}
+		}
+	} while(verticesNaoColoridos.length && cores.length);
+	return verticesColoridos;
+}
